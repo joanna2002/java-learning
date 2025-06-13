19011d523bdf4bad3c1ad261f4140f97787ed60a
joanna2002 <joannacarlaestrella@gmail.com>
2025-06-13 12:17:25 +0200

    Variables

---
+++ 01-core-java/java-language-basics/Arrays/CoffeShop.java
@@ -0,0 +1,69 @@
+import java.util.Arrays;
+
+public class CoffeShop {
+
+    public static void main(String[] args) {
+
+        // 1. Declare and initialize an array
+        String[] coffeeMenu = {
+                "Affogato", "Americano", "Cappuccino", "Corretto", "Cortado",
+                "Doppio", "Espresso", "Frappuccino", "Freddo", "Lungo"
+        };
+
+        // 2. print the full menu using a for loop
+        for (int i = 0; i < coffeeMenu.length; i++) {
+            System.out.println(coffeeMenu[i]);
+        }
+
+        System.out.println();
+
+        // 3. copy part of the menu
+        String[] featuredMenu = new String[5];
+
+        System.arraycopy(coffeeMenu, 0, featuredMenu, 0, 5);
+
+        // print the featuredMenu array
+        for (int i = 0; i < featuredMenu.length; i++) {
+            System.out.println(featuredMenu[i]);
+        };
+
+        System.out.println();
+
+        // 4. copy last 3 coffee names in a new array and print
+        String[] onSale = Arrays.copyOfRange(coffeeMenu, 7, 10);
+
+        for (int i = 0; i < onSale.length; i++) {
+            System.out.println(onSale[i]);
+        };
+
+        System.out.println();
+
+        // 5. sort the menu alphabetically
+        Arrays.sort(coffeeMenu);
+        System.out.println("Sorted Menu: " + Arrays.toString(coffeeMenu));
+
+        System.out.println();
+
+        // 6. Search for a coffee
+        System.out.println("Cappuccino can be found at index: " + Arrays.binarySearch(coffeeMenu, "Cappuccino"));
+
+        System.out.println();
+
+        // 7. multidimensional array
+        String[][] menuDetails = {
+                {"Espresso", "Latte", "Mocha"},
+                {"Small", "Medium", "Large"}
+        };
+
+        System.out.println("Menu Combinations: ");
+        for (String coffeeType : menuDetails[0]) {
+            for (String coffeeSize : menuDetails[1]) {
+                System.out.println(coffeeType + " " + coffeeSize);
+            }
+        }
+
+
+
+
+    }
+}
